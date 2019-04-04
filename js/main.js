932733fd0b19f9fa5de7571a012bdfde5ceb97bc
Student <student@CVS-R212D010.nic.bc.ca>
2019-04-04 12:08:32 -0700

    steve's magic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
+   
+    $(document).on('click',".lockIphone",function(){
+        $('.navmenu-fixed-right').removeClass('in');
+        $('body').removeClass('lockIphone');
+    });
 
-	
+	$('.navbar-settings').click(function(){
+        if(!$('.navmenu-fixed-right').hasClass('in')){
+            $('.navmenu-fixed-right').addClass('in');
+            
+            setTimeout(function(){
+                $('body').addClass('lockIphone');
+            }, 500);
 
+        } else {
+            $('.navmenu-fixed-right').removeClass('in');
+            $('body').removeClass('lockIphone');    
+        }
+
+    });
+
+    
 
 });
